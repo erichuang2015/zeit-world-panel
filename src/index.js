7daf466c6e05db91896b661cb666bd0160fc126d
SukkaW <isukkaw@gmail.com>
2018-09-06 12:50:15 +0800

    feat(zone): change action button style

--- src/index.js
+++ src/index.js
@@ -102,7 +102,19 @@ function getDomainList() {
                     <td class="sk-text-bold"><a href="/record/?domain=${value.name}">${value.name}</a></td>
                     <td>${value.serviceType}</td>
                     <td>
-                        <button data-id="${value.name}" id="${value.name}-del-btn" onclick="confirmDeleteDomain(this)" type="button" class="btn sk-bg-error sk-text-light">DELETE</button>
+                    <button type="button" class="btn btn-link sk-p-0" data-id="${value.name}" data-type="${value.type}" id="${value.name}-del-btn" onclick="confirmDeleteDomain(this)">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path fill="none" d="M0 0h24v24H0V0z" />
+                            <path fill="#e85600" d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"/>
+                            <path fill="none" d="M0 0h24v24H0z" />
+                        </svg>
+                    </button>
+                    <a class="btn btn-link sk-p-0" href="/record/?domain=${value.name}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path fill="#00ad9f" d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
+                            <path d="M0 0h24v24H0z" fill="none"/>
+                        </svg>
+                    </a>
                     </td>
                     <td>${new Date(value.created).toLocaleDateString()}</td>
                 </tr>
